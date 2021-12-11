14cbeae0dda650f5e45d1794f5700f23a0c9528c
Wayne Gibson <wayne.gibson@spacecomx.com>
2021-12-11 04:16:33 -0500

    fixed casks adn removed unused casks

--- update-brewfile.sh
+++ update-brewfile.sh
@@ -12,11 +12,11 @@ echo "Updating brew..."
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-echo "Installing the brew file dependencies."
+echo "Installing the brew file dependencies..."
 brew tap homebrew/bundle
 brew bundle
 
-echo "Cleaning up brew files..."
+echo "Cleaning up brew deprecated formulae..."
 brew cleanup
 
-echo "Completed installing the brew file dependencies..."
+echo "Completed brew file install..."
