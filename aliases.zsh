464e4748a9c2a09b2e3e2bc9e143d46f21805732
Wayne Gibson <wayne.gibson@spacecomx.com>
2022-02-06 02:28:26 -0500

    updated alias name

--- aliases.zsh
+++ aliases.zsh
@@ -6,8 +6,8 @@ alias brew:upgrade="brew update && brew upgrade --greedy && brew cleanup"
 alias cl="clear"
 alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
-alias fresh:zsh="source $HOME/.zshrc"
-alias fresh:dns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias zfresh="source $HOME/.zshrc"
+alias dfresh="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 
 # Filesystem
@@ -26,9 +26,9 @@ alias lara="sites && cd laravel/"
 alias lara:docs="lara && cd docs/"
 
 # Laravel
-alias a="php artisan"
-alias fresh="php artisan migrate:fresh --seed"
+alias art="php artisan"
 alias seed="php artisan db:seed"
+alias sfresh="php artisan migrate:fresh --seed"
 
 # PHP
 alias cfresh="rm -rf vendor/ composer.lock && composer i"
